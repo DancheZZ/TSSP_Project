b253807329a9bc82f3b451fc47900b787a36ad02
Даниил Чернюк <meggi.mo@yandex.ru>
2020-05-31 18:53:17 +0300

    созданы все классы, необходимые в работе ( прописаны конструкторы, поля и пустые методы), завершен внешний вид главного меню

--- parents.cs
+++ parents.cs
@@ -8,8 +8,40 @@ namespace TSSP_V3
 {
     class parents
     {
-        string Family;
-        string Name;
-        string Password;
+        public static int CountParents = 200;
+        int ID;
+        string Family1;
+        string Name1; //фамилия и имя первого родителя
+        string Family2;
+        string Name2;// фамилия и имя второго родителя
+        string Password; //пароль
+        int ID_child; //ID ребенка
+ public string NoteAboutChildren; //замечание о ребенке
+        parents(string F1,string F2,string N1, string N2, string P, int id_child)
+        {
+            Family1 = F1;
+            Name1 = N1;
+            Family2 = F2;
+            Name2 = N2;
+            Password = P;
+            ID = CountParents;//присваиваем айдишник текущему последнему
+            CountParents++;//увеличиваем последний айди
+            ID_child = id_child;
+            NoteAboutChildren = "Замечаний нет";
+        }
+
+    public void TakeNote (string Note)
+    {
+      NoteAboutChildren = Note;
+    }
+
+    public void Look() // сделать возвр. тип int[]
+    {
+       
+    }
+
+
+
+
     }
 }
